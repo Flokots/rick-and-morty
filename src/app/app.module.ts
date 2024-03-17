bd47a5fec0892141bc7315672a724586557119c4
Flokots <florencekotohoyoh@gmail.com>
2024-03-17 16:45:00 +0100

    Create home page outline

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,15 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, 
+    HomeComponent, 
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
