729d0c498629cc19c937b45ecbc2d18cf30a2b4a
Flokots <florencekotohoyoh@gmail.com>
2024-03-22 09:11:26 +0100

    Fix activated route bug in home component

--- src/app/components/home/home.component.spec.ts
+++ src/app/components/home/home.component.spec.ts
@@ -1,7 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './home.component';
-import { ActivatedRoute } from '@angular/router';
+import { RouterTestingModule } from "@angular/router/testing";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -9,7 +10,9 @@ describe('HomeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ HttpClientModule],
+      imports: [ HttpClientModule,
+        RouterTestingModule,
+        HttpClientTestingModule],
       declarations: [HomeComponent]
     })
     .compileComponents();
